f99d16a1f6c789b841409d64c7e036b8db77ab3d
Francis Rousseau <francis.rousseau32@gmail.com>
2023-05-23 17:08:13 +0200

    fix upload

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss']
 })
-export class UploadComponent {
+export class UploadComponent implements OnInit {
+  imageUploaded = false;
+  fileType = 'image/png';
+  fileName = '';
+  diseases = [{
+    name: 'Addisons Disease',
+    probability: 75,
+    color: 'green'
+  },
+  {
+    name: 'Cushing Syndrome',
+    color: 'red',
+    probability: 20
+  },
+  {
+    name: 'Diabetes Mellitus',
+    color: 'light-green',
+    probability: 60
+  },
+  {
+    name: 'Hyperthyroidism',
+    color: 'light-red',
+    probability: 40
+  },
+  {
+    name: 'Hypothyroidism',
+    color: 'orange',
+    probability: 50
+  }]
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  onFileSelected(event: any) {
+    this.imageUploaded = true;
+    const file:File = event.target.files[0];
+
+    if (file) {
+
+        this.fileName = file.name;
+
+        const formData = new FormData();
+
+        formData.append("thumbnail", file);
+
+        const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+        upload$.subscribe();
+    }
+}
 }
--- src/app/upload/upload.module.ts
+++ src/app/upload/upload.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UploadComponent} from "./upload.component";
+import { MatIconModule } from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @NgModule({
   declarations: [UploadComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    HttpClientModule
   ],
   exports: [UploadComponent]
 })
