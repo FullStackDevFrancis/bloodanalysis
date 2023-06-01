af35ba6806178f99ece7528db63b7dd36c610e5f
Francis Rousseau <francis.rousseau32@gmail.com>
2023-06-01 10:32:51 +0200

    styling fixes

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MainComponent} from "./main.component";
 import {MatButtonModule} from "@angular/material/button";
+import {RouterLink} from "@angular/router";
 
 
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [
-    CommonModule,
-    MatButtonModule
-  ],
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        RouterLink
+    ],
   exports: [MainComponent]
 })
 export class MainModule { }
